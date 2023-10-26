c5113735ee29c9cebe916d805726894b1a191cb6
Pavao Dombaj <pavaodombaj@gmail.com>
2023-10-26 08:41:50 +0200

    prva promjena

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ app.get("/", (request, response) => {
     response.send("I am alive!");
 });
 
+app.get("/novo", (request, response)=> {
+    response.send("I am new!");
+});
 app.get("/proba/:name", (request, response) => {
     response.send("Hello, " + request.params.name);
 });
